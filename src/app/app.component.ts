172dbe3a7916bca0267157a3de2e81666a683806
Albert Brząkała <alberrtbrzakala@tlen.pl>
2022-09-13 00:37:05 +0200

    toDoApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDoList';
+  title = 'Lista zadań';
+  tasks: Task[] = [];
+  taskName = '';
+  deadline = '';
+  taskDone = false;
+
+  createTask() {
+    const task: Task = {
+      name: this.taskName,
+      deadline: this.deadline,
+      done: false,
+    };
+    this.tasks.push(task);
+    this.taskName = '';
+    this.deadline = '';
+    this.taskDone = false;
+  }
+
+  clearTask() {
+    this.tasks = [];
+  }
+
+  markTaskAsDone(task: Task) {
+    task.done = true;
+  }
+  stringNotNull() {
+    if (this.taskName != '' && this.deadline != '')
+      return false;
+    else
+      return true;
+  }
+  removeTask(task: Task)
+  {
+    this.tasks = this.tasks.filter(x=> x !== task);
+  }
 }
